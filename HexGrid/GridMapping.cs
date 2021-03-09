0d1cd4b033cbcf81676432ea37e82c70ebe4be0d
tk-yoshimura <yoshimura8192@gmail.com>
2021-03-10 00:26:33 +0900

    + mapping

---
+++ HexGrid/GridMapping.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace HexGrid {
+    /// <summary>Grid Mapping</summary>
+    public class GridMapping {
+        private readonly int[,] map;
+        private readonly bool is_even;
+
+        /// <summary>Width</summary>
+        public double Width { get; private set; }
+        
+        /// <summary>Height</summary>
+        public double Height { get; private set; }
+
+        /// <summary>MakeInstance</summary>
+        public GridMapping(Grid grid) {
+            if (grid.Count < 1) {
+                throw new ArgumentException();
+            }
+
+            this.map = grid.Map;
+            this.is_even = (grid[0].X + grid[0].Y) % 2 == 0;
+            this.Width = grid.MapWidth * 0.5;
+            this.Height = grid.MapHeight;
+        }
+
+        /// <summary>Mapped Cell Index</summary>
+        public int this[double u, double v]{
+            get{
+                if (!InRange(u, v)) {
+                    return Cell.None;
+                }
+
+                int x = (int)Math.Floor(u * 2);
+                int y = (int)Math.Floor(v);
+
+                x = ((y % 2 == 0) ^ is_even) ? ((x - 1) | 1) : (x & ~1);
+
+                if (x < 0 || x >= map.GetLength(0) || y >= map.GetLength(1)) {
+                    return Cell.None;
+                }
+
+                return map[x, y];
+            }
+        }
+
+        /// <summary>Judgement in range</summary>
+        public bool InRange(double u, double v) {
+            return u >= 0 && u <= Width && v >= 0 && v <= Height;
+        }
+    }
+}
---
+++ HexGridTests/GridMappingTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HexGrid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexGrid.Tests {
+    [TestClass()]
+    public class GridMappingTests {
+        [TestMethod()]
+        public void GridMappingTest() {
+            foreach (Grid grid in new Grid[] { 
+                new RectGrid(2, 2), new RectGrid(3, 2), new RectGrid(2, 3), new RectGrid(3, 3), 
+                new TriGrid(2), new TriGrid(3), new HexaGrid(2), new HexaGrid(3) }) {
+                
+                GridMapping mapping = new GridMapping(grid);
+
+                Console.WriteLine(grid.ToString());
+
+                for (double v = 0; v <= mapping.Height; v += 0.5) { 
+                    for (double u = 0; u <= mapping.Width; u += 0.5) {
+                        Console.WriteLine($"{u:F1},{v:F1} : {mapping[u, v]}");
+                    }
+
+                    Console.WriteLine("");
+                }
+
+                Console.WriteLine("---------------------------");
+            }
+        }
+    }
+}
